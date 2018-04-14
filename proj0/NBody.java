f156f3f983b66c1966a6006e25a317c442d9a288
Ed <letitbe0201@gmail.com>
2018-04-14 14:50:48 +0800

    add fucking readPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,4 +6,25 @@ public class NBody {
 
 		return radius;
 	}
+
+	public static Planet[] readPlanets(String filePath) {
+		In in = new In(filePath);
+
+		int planetsNum = in.readInt();
+		double radius = in.readDouble();
+		Planet[] allPlanet = new Planet[planetsNum];
+
+		for(int i = 0; i < planetsNum ; i++) {
+			double xP = in.readDouble();
+			double yP = in.readDouble();
+			double xV = in.readDouble();
+			double yV = in.readDouble();
+			double m = in.readDouble();
+			String name = in.readString();
+
+			Planet p = new Planet(xP,yP,xV,yV,m,name);
+			allPlanet[i] = p;
+		}
+		return allPlanet;
+	}
 }
